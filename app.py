8de213dadf855f99c0434343b6acd2b9d8644ed1
Vasallo94 <65018705+Vasallo94@users.noreply.github.com>
2023-04-02 13:09:50 +0200

    2023

--- app.py
+++ app.py
@@ -403,9 +403,37 @@ def main():
         cols = st.columns(2)
         with cols[0]:
             st.write('Lorem ipsum dolor sit amet...')
+            df_calendar = pd.merge(
+                df_slider, df_calendar, left_on='id', right_index=True)
+            # Leer los datos y convertir la columna 'date' en tipo datetime
+            df_calendar['date'] = pd.to_datetime(df_calendar['date'])
 
+            # Filtrar los datos para tener sólo los disponibles
+            sum_available = df_calendar[df_calendar.available == "t"].groupby(
+                ['date']).size().to_frame(name='available').reset_index()
+
+            # Agregar la columna de día de la semana
+            sum_available['weekday'] = sum_available['date'].dt.day_name()
+
+            # Establecer 'date' como el índice del DataFrame
+            sum_available = sum_available.set_index('date')
+
+            # Crear la figura de Plotly Express
+            disponibilidad = px.line(
+                sum_available, y='Disponible', title='Número de reservas disponibles por fecha', template='plotly_dark')
+            st.plotly_chart(disponibilidad, use_container_width=True)
         with cols[1]:
             st.write('Lorem ipsum dolor sit amet...')
+            average_price = df_calendar[(df_calendar.available == "t") & (
+                df_calendar.accommodates == 2)].groupby(['date']).mean().astype(np.int64).reset_index()
+            average_price['weekday'] = average_price['date'].dt.day_name()
+            average_price = average_price.set_index('date')
+            precio = px.line(average_price, x=average_price.index,
+                             y='price_x', title='Precio medio para dos personas')
+            precio.update_traces(text=average_price['weekday'])
+            precio.update_layout(xaxis_title='Fecha',
+                                 yaxis_title='Precio', template='plotly_dark')
+            st.plotly_chart(precio, use_container_width=True)
 
 
 if __name__ == '__main__':
